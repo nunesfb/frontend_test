8e66d511899356c14cd28adae2cd9ab159bd0681
Rafael Martins <rafamarts@gmail.com>
2022-05-18 11:38:35 -0300

    Dockerizando

--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://localhost:3001/sistema'});
+const backURL = process.env.BACK_URL || "http://localhost:3001/sistema";
+const api = axios.create({baseURL: backURL});
 
 export default api;
